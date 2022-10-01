490d1531a357fd4d7b334ed87ad127c7e9e88081
Ana Carolina <anacarolinaarellano@gmail.com>
2022-10-01 15:18:14 -0500

    added lexer with tests (tests can be deleted later)

--- main.py
+++ main.py
@@ -6,21 +6,107 @@
 # ------------------------------------------
 
 # Import Lex and Yacc
+from tkinter.tix import TCL_DONT_WAIT
 from ply.lex import lex
 from ply.yacc import yacc
 
 # Tokens
 reserved = {
     'program' : 'PROGRAM',
+    'class'   : 'CLASS',
+    'var'     : 'VAR',
+    'assign'  : 'ASSIGN',
+    'fun'     : 'FUN',
+    'return'  : 'RETURN',
+    'int'     : 'INT',
+    'float'   : 'FLOAT',
+    'bool'    : 'BOOLEAN',
+    'mod'     : 'MOD',
+    'void'    : 'VOID',
+    'if'      : 'IF',
+    'else'    : 'ELSE',
+    'or'      : 'OR',
+    'and'     : 'AND',
+    'read'    : 'READ',
+    'print'   : 'PRINT',
+    'for'     : 'FOR',
+    'while'   : 'WHILE'
 }
 
 # Token declaration
-tokens = [] + list(reserved.values())
+tokens = [
+    'SEMICOLON',
+    'COLON',
+    'COMMA',
+    'DOT',
+    'PLUS',
+    'MINUS',
+    'MULT',
+    'DIV',
+    'OPARENTHESIS',
+    'CPARENTHESIS',
+    'OBRACKET',
+    'CBRACKET',
+    'OCURLY',
+    'CCURLY',
+    'LESSTHAN',
+    'GREATERTHAN',
+    'NOTEQUAL',
+    'ISEQUAL',
+    'EQUAL',
+    'ID',
+    'CTEINT',
+    'CTEFLOAT',
+    'CTESTRING',
+    'COMMENT'
+] + list(reserved.values())
 
 # Ignore
 t_ignore = ' \t'
 
 # Token matching
+t_SEMICOLON    = r'\;'
+t_COLON        = r'\:'
+t_COMMA        = r'\,'
+t_DOT          = r'\.'
+t_PLUS         = r'\+'
+t_MINUS        = r'-'
+t_MULT         = r'\*'
+t_DIV          = r'/'
+t_OPARENTHESIS = r'\('
+t_CPARENTHESIS = r'\)'
+t_OBRACKET     = r'\['
+t_CBRACKET     = r'\]'
+t_OCURLY       = r'\{'
+t_CCURLY       = r'\}'
+t_LESSTHAN     = r'\<'
+t_GREATERTHAN  = r'\>'
+t_NOTEQUAL     = r'!='
+t_ISEQUAL      = r'=='
+t_EQUAL        = r'\='
+
+def t_ID(t):
+    r'[a-zA-Z_][a-zA-Z_0-9]*'
+    t.type = reserved.get(t.value,'ID') 
+    return t
+
+def t_CTEFLOAT(t):
+    r'\d+\.\d+'
+    t.value = float(t.value) 
+    return t
+
+def t_CTEINT(t):
+    r'\d+'
+    t.value = int(t.value)
+    return t
+
+def t_CTESTRING(t):
+    r'[a-zA-Z]'
+    t.value = str(t.value)
+    return t    
+
+# Ignore comments
+t_ignore_COMMENT = r'\//.*'
 
 # Definition of reserved words
 
@@ -39,6 +125,30 @@ def t_error(t):
 # Build the lexer object
 lexer = lex()
     
+#lexer test
+data = '''
+3 + 4 * 10
++ -20 *2
+// comentario
+var
+3.5 != 5.1
+6.2 == 6.2
+4/4
+4 / 4
+id1 = 12
+var id2 = 1.3
+'''
+ 
+# Give the lexer some input
+lexer.input(data)
+ 
+# Tokenize
+while True:
+    tok = lexer.token()
+    if not tok: 
+        break      # No more input
+    print(tok)
+ 
 # Parser
 
 # Functions for each grammar rule
